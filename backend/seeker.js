dcc32eb353214044b9aa584715398788e04a4e82
Igor GVNB <kiraritypical@gmail.com>
2023-02-25 14:44:29 +0300

    Первая версия без возможности сохранения файлов

---
+++ backend/seeker.js
@@ -0,0 +1,23 @@
+// Group Reflection (c) All Rights Reserved 2021—2023
+
+// by GVNB
+
+const prompt = require('prompt-sync')();
+const channel = prompt('Введите название канала.\nУчтите, что на нём должна вестись прямая трансляция.\n\n');
+
+const tmi = require('tmi.js');
+const options = {
+    channels: [ channel ]
+};
+const client = new tmi.Client(options);
+
+console.log(`Monitoring ${options['channels'][0]}...`)
+client.on('message', (channel, userState, message, self) => {
+    if (self) return;
+
+    const timeStamp = new Date(parseInt(userState['tmi-sent-ts'])).toLocaleString();
+
+    console.log(`[${timeStamp}] ${userState.username}: ${message}`);
+});
+
+client.connect();
